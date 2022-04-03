6c5cce4c039a0c664da9b45c34fe1a5a7a3a58c8
Alejandro D'Ambrosio <alejandro.dambrosio@gmail.com>
2022-04-03 12:44:07 -0300

    Initial commit

---
+++ src/main/java/com/fluxitsoft/education/java/nohelloworld/NoHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.fluxitsoft.education.java.nohelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fluxitsoft/education/java/nohelloworld/NoHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluxitsoft.education.java.nohelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
